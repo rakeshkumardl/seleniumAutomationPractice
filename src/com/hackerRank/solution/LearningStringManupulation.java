0e8294a5af897e4b46d5a7a16e33ab3c7242cebc
rakeshkumardl <152713965+rakeshkumardl@users.noreply.github.com>
2024-09-11 16:35:26 +0530

    third Commit

--- src/com/hackerRank/solution/LearningStringManupulation.java
+++ src/com/hackerRank/solution/LearningStringManupulation.java
@@ -9,6 +9,15 @@ public class LearningStringManupulation {
 		System.out.println(name.indexOf("Rakesh"));
 		System.out.println(name.toLowerCase());
 		System.out.println(name.toUpperCase());
+		
+		String msg = "Your transaction id is 1457895";
+		String tid = msg.substring(msg.indexOf("is")+2).trim();
+		int id = Integer.parseInt(tid);
+		String id1 =String.valueOf(id);
+		int id2 =Integer.parseInt(id1);
+		System.out.println(id1);
+		System.out.println(tid);
+		System.out.println(id+" " +id2);
 
 	}
 
